18312a8ae74520bf28d96ac19ad162a2f2310e46
shafidark <safi29317@gmail.com.com>
2020-08-12 15:46:01 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+
+function clock(){
+let fullDate = new Date();
+let hour = fullDate.getHours();
+let mins = fullDate.getMinutes();
+let sec = fullDate.getSeconds();
+
+if(hour < 10){
+  hour = "0" + hour;  
+}
+if(mins < 10){
+    mins = "0" + mins;  
+  }
+  if(sec < 10){
+    sec = "0" + sec;  
+  }
+
+document.getElementById('hour').innerHTML = hour + ":";
+document.getElementById('minute').innerHTML = mins + ":";
+document.getElementById('second').innerHTML = sec;
+}
+setInterval(clock, 1000);
